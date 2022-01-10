8e536b8851354acca61af65b46abb0f8046b3da5
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-10 20:16:15 +0530

    Add cart total amount function

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { CartItemType } from "../../types/CartItemType";
 import CartProduct from "../CartProduct/CartProduct";
+import {Button} from "@mui/material"
 
 type CartPropTypes = {
   products: CartItemType[];
@@ -13,8 +14,12 @@ const Cart: FC<CartPropTypes> = ({
   handleAddToCart,
   removeFromCart,
 }) => {
+  // calculate total amount of the cart
+  const getTotalAmount= (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+
   return (
-    <div className="w-[500px] bg-gray-100 flex flex-col  items-center">
+    <div className="w-[500px]  flex flex-col  items-center">
       <h2 className="text-xl font-bold text-purple-500">Your cart</h2>
       {products.length === 0 ? <p>NO items</p> : null}
       {products?.map((product) => (
@@ -25,6 +30,10 @@ const Cart: FC<CartPropTypes> = ({
           removeFromCart={removeFromCart}
         />
       ))}
+      <div className="flex px-4 items-center justify-between w-full">
+      <Button variant="contained" >Proceed to checkout</Button>
+      <h2>Total: ${getTotalAmount(products)}</h2>
+      </div>
     </div>
   );
 };
