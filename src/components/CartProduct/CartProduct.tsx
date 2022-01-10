88e58cdebeda3d3237abe0db48082560050e98ca
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-10 20:17:23 +0530

    Add stylings

--- src/components/CartProduct/CartProduct.tsx
+++ src/components/CartProduct/CartProduct.tsx
@@ -13,10 +13,21 @@ const CartProduct: FC<CartProductProps> = ({
   removeFromCart,
 }) => {
   return (
-    <div>
-      <img src={product.image} alt="okay" className="h-[100px]" />
-      <p>{product.title}</p>
-      <p>{product.price}</p>
+    <div className="flex w-[100%] items-center justify-between m-5 shadow-lg">
+      <div className="flex flex-col w-full p-2 m-2">
+        <h3 className="font-bold text-lg">{product.title}</h3>
+        <div className="flex text-md text-gray-600 font-bold font-mono justify-between items-center p-2" >
+          <p>$ {product.price}</p>
+          <p>Total: ${(product.price * product.amount).toFixed(2)}</p>
+        </div>
+        <div className="flex items-center p-2 justify-between">
+          <button className="bg-red-600 p-2 w-[15%] text-white" onClick={() => removeFromCart(product.id)}>-</button>
+          <span>{product.amount}</span>
+
+          <button className="bg-blue-300 p-2 w-[15%] text-white" onClick={()=>handleAddToCart(product)}>+</button>
+        </div>
+      </div>
+      <img src={product.image} alt="okay" className="h-[150px] object-contain m-2" />
     </div>
   );
 };
